005f4a6914c0dd8d5103db964e09af31f41d7515
harshasatpute <harshasatpute8@gmail.com>
2025-09-25 13:48:38 +0530

    new commit

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -5,4 +5,4 @@ set -e
 docker pull 9764080588/simple-python-flask-app
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 98764080588/simple-python-flask-app
+docker run -d -p 5000:5000 9764080588/simple-python-flask-app
